a9393d802aa3ae124c9675f88c2dacd1c0a93ad1
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-08-07 19:00:30 -0500

    Login terminado

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ var cuentas = [
   { nombre:"Gera", saldo: 290 },
   { nombre: "Maui", saldo: 67 }
 ];
+
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const button = document.getElementById('button-login');
+
+button.addEventListener('click', (e) =>{
+    e.preventDefault()
+    const data = {
+        username: username.value,
+        password: password.value
+    }
+   console.log(data);
+})
