8e5c123f78c7703b6c7d3e9bb2b858d755a99bb4
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-08-07 20:56:12 -0500

    agregando clase hide

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ const cuentas = [
 console.log(cuentas)
 const username = document.getElementById('username');
 const password = document.getElementById('password');
+const formlogin = document.getElementById('form-login');
 const infocuenta = document.getElementById('pag-user');
 
 function login(){
     if (username.value == cuentas[0].nombre && password.value == cuentas[0].clave ) {
+        formlogin.classList.add("hide");
+        infocuenta.classList.remove("hide");
         const titulo = document.createElement('h2');
         titulo.textContent = `Bienvenido ${username.value}`; 
         
         infocuenta.appendChild(titulo);
+
 
     }else if (username.value == cuentas[1].nombre && password.value == cuentas[1].clave ) {
         const titulo = document.createElement('h2');
