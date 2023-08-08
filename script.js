6c445b5d443b4540c624e974bce3946775109ec5
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-08-07 20:36:19 -0500

    Login funcional

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-var cuentas = [
-  { nombre: "Mali", saldo: 200 },
-  { nombre:"Gera", saldo: 290 },
-  { nombre: "Maui", saldo: 67 }
+const cuentas = [
+  { nombre: "Mali", saldo: 200, clave:123456},
+  { nombre:"Gera", saldo: 290, clave:123456},
+  { nombre: "Maui", saldo: 67, clave:123456}
 ];
-
+console.log(cuentas)
 const username = document.getElementById('username');
 const password = document.getElementById('password');
-const button = document.getElementById('button-login');
+const section = document.getElementById('info-cuenta')
 
-button.addEventListener('click', (e) =>{
-    e.preventDefault()
-    const data = {
-        username: username.value,
-        password: password.value
+function login(){
+    if (username.value == cuentas[0].nombre && password.value == cuentas[0].clave ) {
+            
+
+    }else if (username.value == cuentas[1].nombre && password.value == cuentas[1].clave ) {
+        
+
+    }else if (username.value == cuentas[2].nombre && password.value == cuentas[2].clave ) {
+        
+        
+    }else{
+        alert("algo mal")
     }
-   console.log(data);
-})
+    
+}; 
