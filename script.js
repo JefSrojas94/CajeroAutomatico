bb5ae75eaf8d97f4e126fde372fab3472d2af4e6
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-08-10 16:47:22 -0500

    Deposito funcional

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ console.log(cuentas);
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const formlogin = document.getElementById("form-login");
+
 const infocuenta = document.getElementById("pag-user");
 const saldos = document.getElementById("saldocuenta");
+const input = document.getElementById("movimientos-cuenta");
+const botonDeposito = document.getElementById("btn-deposito");
+
+
 
 
 
@@ -26,6 +31,23 @@ function login() {
     saldoActual.className = "saldo";
     saldoActual.innerHTML = `Su saldo actual es ${cuentas[0].saldo}`;
 
+    botonDeposito.addEventListener('click', function(e){
+      console.log(e);
+      //tomar cantidad a depositar
+      const deposito = input.value;
+      //convertir a numero
+      const numDeposito = Number(deposito);
+      
+      //saber cuanto hay en su cuenta
+      const saldoCuenta = cuentas[0].saldo;
+      //sumarlo con el deposito
+      const nuevoSaldo = numDeposito+saldoCuenta;
+      //mostrar en el elemento
+      //mostrar en el label el saldo de la cuenta
+      cuentas[0].saldo = nuevoSaldo;
+      saldoActual.innerText = `Su saldo actual es: ${cuentas[0].saldo}`;
+    })
+
     saldos.appendChild(saldoActual);
   } else if (
     username.value == cuentas[1].nombre &&
@@ -34,6 +56,29 @@ function login() {
     formlogin.classList.add("hide");
     infocuenta.classList.remove("hide");
     document.getElementById('tituloBienvenida').innerHTML = `Bienvenido ${username.value}`;
+
+    let saldoActual = document.createElement('label');
+    saldoActual.className = "saldo";
+    saldoActual.innerHTML = `Su saldo actual es ${cuentas[1].saldo}`;
+
+    botonDeposito.addEventListener('click', function(e){
+      console.log(e);
+      //tomar cantidad a depositar
+      const deposito = input.value;
+      //convertir a numero
+      const numDeposito = Number(deposito);
+      
+      //saber cuanto hay en su cuenta
+      const saldoCuenta = cuentas[1].saldo;
+      //sumarlo con el deposito
+      const nuevoSaldo = numDeposito+saldoCuenta;
+      //mostrar en el elemento
+      //mostrar en el label el saldo de la cuenta
+      cuentas[1].saldo = nuevoSaldo;
+      saldoActual.innerText = `Su saldo actual es: ${cuentas[1].saldo}`;
+    })
+
+    saldos.appendChild(saldoActual);
   } else if (
     username.value == cuentas[2].nombre &&
     password.value == cuentas[2].clave
@@ -41,6 +86,29 @@ function login() {
     formlogin.classList.add("hide");
     infocuenta.classList.remove("hide");
     document.getElementById('tituloBienvenida').innerHTML = `Bienvenido ${username.value}`;
+
+    let saldoActual = document.createElement('label');
+    saldoActual.className = "saldo";
+    saldoActual.innerHTML = `Su saldo actual es ${cuentas[2].saldo}`;
+
+    botonDeposito.addEventListener('click', function(e){
+      console.log(e);
+      //tomar cantidad a depositar
+      const deposito = input.value;
+      //convertir a numero
+      const numDeposito = Number(deposito);
+      
+      //saber cuanto hay en su cuenta
+      const saldoCuenta = cuentas[2].saldo;
+      //sumarlo con el deposito
+      const nuevoSaldo = numDeposito+saldoCuenta;
+      //mostrar en el elemento
+      //mostrar en el label el saldo de la cuenta
+      cuentas[2].saldo = nuevoSaldo;
+      saldoActual.innerText = `Su saldo actual es: ${cuentas[2].saldo}`;
+    })
+
+    saldos.appendChild(saldoActual);
   } else {
     alert("Usuario o contraseña incorrectos");
   }
