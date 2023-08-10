25bdda75c7c8dd532bd90c213edc4ecc05e9c0ca
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-08-10 16:56:55 -0500

    Retiro validado y funcional

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const infocuenta = document.getElementById("pag-user");
 const saldos = document.getElementById("saldocuenta");
 const input = document.getElementById("movimientos-cuenta");
 const botonDeposito = document.getElementById("btn-deposito");
+const botonRetiro = document.getElementById("btn-retiro");
 
 
 
@@ -30,7 +31,7 @@ function login() {
     let saldoActual = document.createElement('label');
     saldoActual.className = "saldo";
     saldoActual.innerHTML = `Su saldo actual es ${cuentas[0].saldo}`;
-
+    //BOTON DEPOSITO
     botonDeposito.addEventListener('click', function(e){
       console.log(e);
       //tomar cantidad a depositar
@@ -47,6 +48,29 @@ function login() {
       cuentas[0].saldo = nuevoSaldo;
       saldoActual.innerText = `Su saldo actual es: ${cuentas[0].saldo}`;
     })
+
+    //BOTON RETIRO
+    botonRetiro.addEventListener('click', function(e){
+      console.log(e);
+      //tomar cantidad a retirar
+      const retiro = input.value;
+      //convertir a numero
+      const numRetiro = Number(retiro);
+      
+      //saber cuanto hay en su cuenta
+      const saldoCuenta = cuentas[0].saldo;
+      //validar si el monto es menor que el saldo
+      if (numRetiro < saldoCuenta) {
+        //restarlo con el retiro
+         const nuevoSaldo = saldoCuenta-numRetiro;
+         cuentas[0].saldo = nuevoSaldo;
+      }else alert("Monto a retirar mayor al saldo")
+      
+      //mostrar en el elemento
+      //mostrar en el label el saldo de la cuenta
+      saldoActual.innerText = `Su saldo actual es: ${cuentas[0].saldo}`;
+    });
+
 
     saldos.appendChild(saldoActual);
   } else if (
@@ -76,7 +100,29 @@ function login() {
       //mostrar en el label el saldo de la cuenta
       cuentas[1].saldo = nuevoSaldo;
       saldoActual.innerText = `Su saldo actual es: ${cuentas[1].saldo}`;
-    })
+    });
+
+    //BOTON RETIRO
+    botonRetiro.addEventListener('click', function(e){
+      console.log(e);
+      //tomar cantidad a retirar
+      const retiro = input.value;
+      //convertir a numero
+      const numRetiro = Number(retiro);
+      
+      //saber cuanto hay en su cuenta
+      const saldoCuenta = cuentas[1].saldo;
+      //validar si el monto es menor que el saldo
+      if (numRetiro < saldoCuenta) {
+        //restarlo con el retiro
+         const nuevoSaldo = saldoCuenta-numRetiro;
+         cuentas[1].saldo = nuevoSaldo;
+      }else alert("Monto a retirar mayor al saldo")
+      
+      //mostrar en el elemento
+      //mostrar en el label el saldo de la cuenta
+      saldoActual.innerText = `Su saldo actual es: ${cuentas[1].saldo}`;
+    });
 
     saldos.appendChild(saldoActual);
   } else if (
@@ -106,7 +152,29 @@ function login() {
       //mostrar en el label el saldo de la cuenta
       cuentas[2].saldo = nuevoSaldo;
       saldoActual.innerText = `Su saldo actual es: ${cuentas[2].saldo}`;
-    })
+    });
+
+    //BOTON RETIRO
+    botonRetiro.addEventListener('click', function(e){
+      console.log(e);
+      //tomar cantidad a retirar
+      const retiro = input.value;
+      //convertir a numero
+      const numRetiro = Number(retiro);
+      
+      //saber cuanto hay en su cuenta
+      const saldoCuenta = cuentas[2].saldo;
+      //validar si el monto es menor que el saldo
+      if (numRetiro < saldoCuenta) {
+        //restarlo con el retiro
+         const nuevoSaldo = saldoCuenta-numRetiro;
+         cuentas[2].saldo = nuevoSaldo;
+      }else alert("Monto a retirar mayor al saldo")
+      
+      //mostrar en el elemento
+      //mostrar en el label el saldo de la cuenta
+      saldoActual.innerText = `Su saldo actual es: ${cuentas[2].saldo}`;
+    });
 
     saldos.appendChild(saldoActual);
   } else {
