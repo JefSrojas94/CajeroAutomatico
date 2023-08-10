357cb49ab95ef9aab07179ca5630bafc87e6556f
Jeffry Rojas <jeffrysnneyder.rr@gmail.com>
2023-08-10 15:30:44 -0500

    Segundo formulario

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 const cuentas = [
-  { nombre: "Mali", saldo: 200, clave:123456},
-  { nombre:"Gera", saldo: 290, clave:123456},
-  { nombre: "Maui", saldo: 67, clave:123456}
+  { nombre: "Mali", saldo: 200, clave: 123456 },
+  { nombre: "Gera", saldo: 290, clave: 123456 },
+  { nombre: "Maui", saldo: 67, clave: 123456 },
 ];
-console.log(cuentas)
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const formlogin = document.getElementById('form-login');
-const infocuenta = document.getElementById('pag-user');
+console.log(cuentas);
 
-function login(){
-    if (username.value == cuentas[0].nombre && password.value == cuentas[0].clave ) {
-        formlogin.classList.add("hide");
-        infocuenta.classList.remove("hide");
-        const titulo = document.createElement('h2');
-        titulo.textContent = `Bienvenido ${username.value}`; 
-        
-        infocuenta.appendChild(titulo);
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const formlogin = document.getElementById("form-login");
+const infocuenta = document.getElementById("pag-user");
+const saldos = document.getElementById("saldocuenta");
 
 
-    }else if (username.value == cuentas[1].nombre && password.value == cuentas[1].clave ) {
-        const titulo = document.createElement('h2');
-        titulo.textContent = `Bienvenido ${username.value}`; 
-        
-        infocuenta.appendChild(titulo); 
 
-    }else if (username.value == cuentas[2].nombre && password.value == cuentas[2].clave ) {
-        const titulo = document.createElement('h2');
-        titulo.textContent = `Bienvenido ${username.value}`; 
-        
-        infocuenta.appendChild(titulo);
+function login() {
+  if (
+    username.value == cuentas[0].nombre &&
+    password.value == cuentas[0].clave
+  ) {
+    formlogin.classList.add("hide");
+    infocuenta.classList.remove("hide");
+    document.getElementById('tituloBienvenida').innerHTML = `Bienvenido ${username.value}`; 
 
-    }else{
-        alert("algo mal")
-    }
-    
-}; 
+    let saldoActual = document.createElement('label');
+    saldoActual.className = "saldo";
+    saldoActual.innerHTML = `Su saldo actual es ${cuentas[0].saldo}`;
+
+    saldos.appendChild(saldoActual);
+  } else if (
+    username.value == cuentas[1].nombre &&
+    password.value == cuentas[1].clave
+  ) {
+    formlogin.classList.add("hide");
+    infocuenta.classList.remove("hide");
+    document.getElementById('tituloBienvenida').innerHTML = `Bienvenido ${username.value}`;
+  } else if (
+    username.value == cuentas[2].nombre &&
+    password.value == cuentas[2].clave
+  ) {
+    formlogin.classList.add("hide");
+    infocuenta.classList.remove("hide");
+    document.getElementById('tituloBienvenida').innerHTML = `Bienvenido ${username.value}`;
+  } else {
+    alert("Usuario o contraseña incorrectos");
+  }
+}
